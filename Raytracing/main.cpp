73208a041e900957ecf9760cba68b0a881510b04
ocornet <olivier.cornet@eleves.ec-nantes.fr>
2019-02-04 16:30:26 +0100

    Now outputs ppm image + vec3 added

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include "vec3.h"
 
 using namespace std;
 
 int main(int, char*) {
+	ofstream file;
+	file.open("output.ppm");
 	int nx = 200;
 	int ny = 100;
-	cout << "P3\n" << nx << " " << ny << "\n255\n";
+	file << "P3\n" << nx << " " << ny << "\n255\n";
+	//(i,j) = (0,0) en  bas à gauche
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
-			std::cout << ir << " " << ig << " " << ib << endl;
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
+			file << ir << " " << ig << " " << ib << endl;
 
 		}
 	}
+
+	file.close();
 	int end;
 	cin >> end;
 	return 0;
