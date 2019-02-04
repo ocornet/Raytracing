73165428aadf0bce831e91d6806ad9a830d697db
ocornet <olivier.cornet@eleves.ec-nantes.fr>
2019-02-04 17:55:31 +0100

    Add a red ball in the vision

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -10,6 +10,25 @@ vec3 color(const ray& r) {
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
+bool hit_sphere(const vec3& centre, float radius, const ray& r) {
+
+	vec3 oc = r.origin() - centre;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(r.direction(), oc);
+	float c = dot(oc, oc) - radius * radius;
+	return (b*b - 4 * a*c) > 0;
+}
+
+vec3 colorSphere(const ray&r) {
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {  // Si le rayon touche le ballon
+		return vec3(1, 0, 0);  // Tout rough
+	}
+	vec3 unit_direction = unit_vector(r.direction());  // S'il ne le touche pas
+	float t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);  // Couleur normale
+}
+
+
 
 int main(int, char*) {
 	ofstream file;
@@ -31,7 +50,7 @@ int main(int, char*) {
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 col = color(r);
+			vec3 col = colorSphere(r);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
