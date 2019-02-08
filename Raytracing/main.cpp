8042deaaf2a586451001190e663f9f93fe006fd0
ocornet <olivier.cornet@eleves.ec-nantes.fr>
2019-02-08 12:31:53 +0100

    change colour by the valeur of normal, from x,y,z to r,g,b

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -10,21 +10,33 @@ vec3 color(const ray& r) {
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
-bool hit_sphere(const vec3& centre, float radius, const ray& r) {
+float hit_sphere(const vec3& centre, float radius, const ray& r) {
 
 	vec3 oc = r.origin() - centre;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0 * dot(r.direction(), oc);
 	float c = dot(oc, oc) - radius * radius;
-	return (b*b - 4 * a*c) > 0;
+	float discirminant = (b*b - 4 * a*c);
+	if (discirminant < 0) {
+		return -1.0;
+	}
+	else {
+		return (-b - sqrt(discirminant)) / (2.0 * a);  // pas + , puisque - est le premier point qui est touché
+	}
+
 }
 
 vec3 colorSphere(const ray&r) {
-	if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {  // Si le rayon touche le ballon
-		return vec3(1, 0, 0);  // Tout rough
+
+	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+	if (t > 0.0) {  // Si le rayon touche le ballon
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1)); 
+		return 0.5* vec3( N.x() + 1, N.y() + 1, N.z() + 1 );         // Changement de couleur selon (R,G,B)
 	}
+	
+	
 	vec3 unit_direction = unit_vector(r.direction());  // S'il ne le touche pas
-	float t = 0.5*(unit_direction.y() + 1.0);
+	t = 0.5*(unit_direction.y() + 1.0);
 	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);  // Couleur normale
 }
 
@@ -41,8 +53,6 @@ int main(int, char*) {
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
-
-
 
 	//(i,j) = (0,0) en  bas à gauche
 	for (int j = ny - 1; j >= 0; j--) {
